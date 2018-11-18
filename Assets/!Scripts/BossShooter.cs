02580997b6cca7a93ba1b3992112eaf62a375ad3
Johan Tan <johanaldrictan@gmail.com>
2018-11-18 15:55:55 -0800

    Started dummy boss script to test for ability casts Started SkillEvent script to track skill usage

---
+++ Assets/!Scripts/BossShooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dummy script in place for boss AI
+public class BossShooter : MonoBehaviour {
+
+    Actor _parentActor;
+
+	// Use this for initialization
+	void Start () {
+        _parentActor = GetComponent<Actor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //shoot at a random interval
+        if(Time.time % 10 == 0)
+        {
+            _parentActor.CombatController.Skills.TryUse(1);
+        }
+	}
+}
