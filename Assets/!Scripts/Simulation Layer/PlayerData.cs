2b2532018c00c8fa3fbd5f1992b7b0e36c49411b
Johan Tan <johanaldrictan@gmail.com>
2018-12-05 16:42:56 -0800

    added transform property to PlayerData

--- Assets/!Scripts/Simulation Layer/PlayerData.cs
+++ Assets/!Scripts/Simulation Layer/PlayerData.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerData : MonoBehaviour {
+    //HUGE WARNING: This class needs to be updated as needed in order to track everything that needs to be tracked for the SaveData
 
-	
+    //Temporary value to test save data, delete later
+    public int[] Transform;
 }
